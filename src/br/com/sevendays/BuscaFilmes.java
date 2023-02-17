712dc66ab3c99a984eca26816a000a3a29f794ac
GiovanniKF <giiovanni.kf@gmail.com>
2023-02-17 14:52:09 -0300

    Desafio 7DaysOfCode - Dia 1

---
+++ src/br/com/sevendays/BuscaFilmes.java
@@ -0,0 +1,31 @@
+package br.com.sevendays;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class BuscaFilmes {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_l4zt2oir"))
+                .timeout(Duration.ofMinutes(1))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONObject jsonObject = new JSONObject(response.body());
+        System.out.println(response.statusCode());
+        System.out.println(jsonObject.toString(4));
+    }
+
+}
