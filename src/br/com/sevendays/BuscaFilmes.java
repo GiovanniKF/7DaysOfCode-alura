9814235e629e59d96d1c14d8c9f19f53e3816b7c
GiovanniKF <giiovanni.kf@gmail.com>
2023-02-17 14:58:17 -0300

    Desafio 7DaysOfCode - Dia 1

--- src/br/com/sevendays/BuscaFilmes.java
+++ src/br/com/sevendays/BuscaFilmes.java
@@ -26,6 +26,7 @@ public class BuscaFilmes {
         JSONObject jsonObject = new JSONObject(response.body());
         System.out.println(response.statusCode());
         System.out.println(jsonObject.toString(4));
+
     }
 
 }
